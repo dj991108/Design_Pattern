b829e991ed5ea37a11321f89711d72c3c37d4424
DongJun Kim <dj991108@naver.com>
2022-09-11 12:07:38 +0900

    Singleton

--- Singleton_Ticket/src/TicketManager.java
+++ Singleton_Ticket/src/TicketManager.java
@@ -7,6 +7,7 @@ public class TicketManager {
         count = 0;
     }
 
+    public static TicketManager instance; // Lazy Initialization
     public static TicketManager getInstance(){ //인스턴스 생성 메소드 생성
         if(instance == null){
             instance = new TicketManager();
@@ -17,4 +18,9 @@ public class TicketManager {
         if (count >= LIMITS) return Optional.empty();
         return Optional.of(new Ticket(count++));
     }
+    /*
+    Optional<T> : null이 올 수 있는 값을 감싸는 Wrapper 클래스 (NPE를 방지)
+    - 메소드 -
+    .empty()  /  .of(value)  / .ofNullbale(value)
+     */
 }
