7404405893b7c6c0c90aae3119ec7fe6c41a60ed
DongJun Kim <dj991108@naver.com>
2022-09-16 23:51:29 +0900

    Singleton

--- Singleton/src/Printer.java
+++ Singleton/src/Printer.java
@@ -2,9 +2,9 @@ public class Printer {
     private static Printer instance;
     private Printer() {}
     public void print(String msg) {
-        System.out.println("+++++++++");
+        System.out.println("+++++++++++");
         System.out.println(msg);
-        System.out.println("+++++++++");
+        System.out.println("+++++++++++");
     }
     public static Printer getInstance(){ //getInstance 라는 고정된 메소드? ,정적메소드, 인스턴스메소드 복습, 클래스메소드?
         if(instance == null)
