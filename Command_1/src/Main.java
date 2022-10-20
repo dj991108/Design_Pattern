3c26cc693eca857393ce9b419c3d80703ddce423
DongJun Kim <dj991108@naver.com>
2022-10-20 18:43:27 +0900

    Command

--- Command_1/src/Main.java
+++ Command_1/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     // Command Pattern
     public static void main(String[] args) {
         TV tv = new TV();
+        // 다운캐스팅
         Command powerCmd = new PowerCommand(tv);
         Command muteCmd = new MuteCommand(tv);
         TwoButtonController rc = new TwoButtonController(muteCmd, powerCmd);
