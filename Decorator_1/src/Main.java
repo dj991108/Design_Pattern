14543662ac5d19712fc0b68d17e5e247e5b94d1b
DongJun Kim <dj991108@naver.com>
2022-11-12 14:06:34 +0900

    Decorator

--- Decorator_1/src/Main.java
+++ Decorator_1/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        EmailContent b = new BasicEmailContent("insang");
-        EmailContent d1 = new SecureEmailContent(b);
+        EmailContent d = new BasicEmailContent("insang");
+        EmailContent d1 = new SecureEmailContent(d);
         EmailContent d2 = new ExternalEmailContent(d1);
         System.out.println(d2.getContent());
 
