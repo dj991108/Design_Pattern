13d55703463de990f8848f891e6650ca5b5ca120
DongJun Kim <dj991108@naver.com>
2022-12-01 23:43:04 +0900

    Brigde

--- Quiz3_1/src/Main.java
+++ Quiz3_1/src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    // brigde
     public static void main(String[] args) {
         CountDisplay d = new CountDisplay(new StringDisplayImpl("Hello, Universe!"));
         d.multiDisplay(5);
