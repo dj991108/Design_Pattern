383ca2b272a126f8c07d029fcf8478e59da6a210
DongJun Kim <dj991108@naver.com>
2022-12-04 18:19:58 +0900

    Adapter

--- Adapter/src/Main.java
+++ Adapter/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
+    // Adapter
     public static void main(String[] args) {
-        Adder adder = new MyAdder();
-        UseAdder use = new UseAdder();
-        System.out.println(use.add(adder, 10, 20));
-        Adder adder1 = new YourAdderAdapter(new YourAdder());
-        System.out.println(use.add(adder1, 10, 20));
+        Adder adder = new MyAdder(); // 기본 adder ( adder : 연걸자, MyAdder : 제공자 )
+        UseAdder use = new UseAdder(); // 실 사용 클래스 (사용자)
+        System.out.println(use.add(adder, 10, 20));  // 사용자가 연결자를 사용
+        Adder adder1 = new YourAdderAdapter(new YourAdder()); // Adapter 패턴을 통해 제공자를 YourAdder로 변경
+        // adder1 : 연결자   ,  YourAdder : 제공자
+        System.out.println(use.add(adder1, 10, 20)); // 사용자가 연결자를 사용
     }
 }
