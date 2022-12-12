bb9b05c8a5a30e7fe7f4728030446ff3895e00af
DongJun Kim <dj991108@naver.com>
2022-12-12 23:32:35 +0900

    Decorator

--- Decorator_2/src/Main.java
+++ Decorator_2/src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    // Decorator pattern
     public static void main(String[] args) {
         Report r = new Sharp(new Star(new BasicReport("Design Pattern")));
         r.displayContent();
