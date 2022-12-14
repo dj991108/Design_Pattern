9319a53ac0bf07cd8b9a59750cd1e7b9eaebd694
DongJun Kim <dj991108@naver.com>
2022-12-14 23:51:10 +0900

    Decorator

--- Decorator_1/src/ExternalEmailContent.java
+++ Decorator_1/src/ExternalEmailContent.java
@@ -5,10 +5,14 @@ public class ExternalEmailContent extends EmailDecorator{
     }
     private String addDisclainer(String content){
         return content + " : disclaner";
-    }
+    } // super 핵심
 
     public String getContent() {
         String content = super.getContent();
+        /*
+        super 핵심 --> 부모인 추상클래스 EmailDecorator의 속성 EmailContent decorate 의
+        getContent()  !!-> 이전단계(상위레벨)에서의 값 호출      ex) Basic의 insang
+         */
         String externalContent = addDisclainer(content);
         return externalContent;
     }
--- Decorator_1/src/Main.java
+++ Decorator_1/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
+    // Decorator : 하나씩 추가기능이 붙는 느낌!!
     public static void main(String[] args) {
         EmailContent d0 = new BasicEmailContent("insang");
         EmailContent d1 = new SecureEmailContent(d0);
         EmailContent d2 = new ExternalEmailContent(d1);
         System.out.println(d2.getContent());
-
     }
 }
